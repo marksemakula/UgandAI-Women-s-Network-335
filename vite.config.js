ff657dc04c8e76a2578568647260ba845272f328
Mark SEMAKULA™ <marksemakula@gmail.com>
2025-05-03 13:30:04 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite';
+iimport { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/', // Changed from './' to '/'
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
-   build: {
+  build: {
     outDir: 'dist',
-    sourcemap: true
-  },
+    sourcemap: true,
+    emptyOutDir: true // Add this to clear old files
+  }
 });
