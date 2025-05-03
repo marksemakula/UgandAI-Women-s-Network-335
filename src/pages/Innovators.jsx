624bc42e3dda475803fb28d787934dcaa7bb277f
Mark SEMAKULA™ <marksemakula@gmail.com>
2025-05-03 21:33:14 +0300

    Update Innovators.jsx

--- src/pages/Innovators.jsx
+++ src/pages/Innovators.jsx
@@ -7,7 +7,7 @@ const innovators = [
     name: "Evelyn Patra Asio, PhD Fellow",
     title: "UX / UI Designer &Lead AI Researcher at Makerere AI Lab",
     image: "/images/Dr.Asio_Evelyn_Patra.png",
-    bio: "Dr. Asio leads groundbreaking research in Tools & AI applications for healthcare and agriculture in Uganda. Her work has revolutionized early disease detection in rural communities.",
+    bio: "Dr. Asio leads groundbreaking research in Tools & AI applications for healthcare and agriculture in Uganda. Her work has revolutionized early disease detection & Surveillance in rural communities.",
     achievements: [
       "Published 15+ research papers in top Data Science journals & AI conferences",
       "Developed AI model for crop, animal & human disease detection projected to be used by 10,000+ farmers",
